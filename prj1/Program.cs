4a2e2dfcb3bbe472879427d49a99ece2ee8b4a59
saegen <saegen@hotmail.com>
2015-10-08 11:20:35 +0200

    Privata; Lokal master commit med konflikt kod. Push i nästa steg

--- prj1/Program.cs
+++ prj1/Program.cs
@@ -12,6 +12,7 @@ namespace prj1
         static void Main(string[] args)
         {
             Debug.WriteLine("WorkComputer");
+            Debug.WriteLine("Privat, Konflikt kod");
         }
     }
 }
