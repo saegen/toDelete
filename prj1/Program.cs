4fdd53354e5d8fe7954b2d525054f31e67c90c1c
saegen <saegen@hotmail.com>
2015-10-08 10:13:37 +0200

    WorkComputer

---
+++ prj1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("WorkComputer");
+        }
+    }
+}
